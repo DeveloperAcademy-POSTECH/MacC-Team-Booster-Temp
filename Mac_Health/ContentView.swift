91e4d7f4efa635a677cb4f54e4557fe50a22ecf7
jinyong <96717313+it794613@users.noreply.github.com>
2023-10-19 18:46:24 +0900

    main

---
+++ Mac_Health/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mac_Health
+//
+//  Created by 최진용 on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mac_Health/Mac_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_HealthApp.swift
+//  Mac_Health
+//
+//  Created by 최진용 on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Mac_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
