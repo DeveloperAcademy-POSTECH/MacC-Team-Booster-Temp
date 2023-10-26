97c6853dbb84db48045a44bbc19d27371cb4ca9c
songmoro <wlqgkr@naver.com>
2023-10-26 14:16:31 +0900

    feat: 플로팅 버튼 수정

--- Mac_Health/Common/Component/FloatingButton.swift
+++ Mac_Health/Common/Component/FloatingButton.swift
@@ -10,30 +10,34 @@ import SwiftUI
 struct FloatingButton<Content: View>: View {
     let backgroundColor: Color
     @ViewBuilder let content: Content
-    let action: () -> ()
     
     var body: some View {
-        Button {
-            action()
-        } label: {
-            RoundedRectangle(cornerRadius: 100)
-                .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(60))
-                .foregroundColor(backgroundColor)
-                .overlay {
-                    content
-                }
-        }
+        RoundedRectangle(cornerRadius: 100)
+            .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(60))
+            .foregroundColor(backgroundColor)
+            .overlay {
+                content
+            }
     }
 }
 
 struct FloatingButton_Previews: PreviewProvider {
     static var previews: some View {
-        FloatingButton(backgroundColor: .gray_600) {
-            Text("세트 완료")
-                .font(.button1())
-                .foregroundColor(.label_900)
-        } action: {
-            print("세트 완료")
+        FloatingButton(backgroundColor: .blue_main) {
+            HStack {
+                Button {
+                    print("버튼 1")
+                } label: {
+                    Image(systemName: "square.and.arrow.down")
+                }
+                Spacer()
+                Button {
+                    print("버튼 2")
+                } label: {
+                    Image(systemName: "square.and.arrow.up")
+                }
+            }
+            .padding(.horizontal)
         }
     }
 }
