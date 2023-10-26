98861c3bfcd37e26a6e073c5401596e449406eab
songmoro <wlqgkr@naver.com>
2023-10-26 17:04:29 +0900

    feat: 운동 기록 뷰 추가

---
+++ Mac_Health/View/RecordView/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 10/26/23.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordView()
+}
