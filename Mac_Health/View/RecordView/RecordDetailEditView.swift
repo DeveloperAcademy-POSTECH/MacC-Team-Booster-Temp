975b8a5db51a5e7ecb242552759511d682e2e468
songmoro <wlqgkr@naver.com>
2023-10-27 21:34:17 +0900

    feat: recordDetailView 작업

---
+++ Mac_Health/View/RecordView/RecordDetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDetailEditView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 10/27/23.
+//
+
+import SwiftUI
+
+struct RecordDetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordDetailEditView()
+}
--- Mac_Health/View/RecordView/RecordDetailView.swift
+++ Mac_Health/View/RecordView/RecordDetailView.swift
@@ -9,10 +9,113 @@ import SwiftUI
 
 struct RecordDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray_900.ignoresSafeArea()
+            
+            VStack {
+                navigation
+                recordCard
+                workoutDetail
+                workoutDetail
+                workoutDetail
+                Spacer()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var navigation: some View {
+        HStack {
+            Image(systemName: "chevron.left")
+                .font(.headline1())
+                .foregroundColor(.label_700)
+            Spacer()
+            
+            Text("2023년 6월 23일")
+                .font(.headline1())
+                .foregroundColor(.white)
+            Spacer()
+            
+            Image(systemName: "pencil")
+                .font(.headline1())
+                .foregroundColor(.label_700)
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    var recordCard: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(.fill_1)
+            .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(72))
+            .overlay {
+                VStack {
+                    HStack {
+                        Ellipse()
+                            .frame(width: UIScreen.getWidth(8), height: UIScreen.getHeight(8))
+                            .foregroundColor(.yellow_main)
+                        Text("박재훈")
+                            .font(.headline2())
+                            .foregroundColor(.label_900)
+                        Spacer()
+                        
+                        Text("1시간 32분 12초")
+                            .font(.headline2())
+                            .foregroundColor(.label_900)
+                    }
+                    Spacer()
+                    HStack {
+                        Text("등/가슴")
+                            .font(.body2())
+                            .foregroundColor(.label_900)
+                        Spacer()
+                        
+                        Text("14500kg")
+                            .font(.body2())
+                            .foregroundColor(.label_900)
+                    }
+                }
+                .padding()
+            }
+    }
+    
+    @ViewBuilder
+    var workoutDetail: some View {
+        VStack {
+            HStack {
+                Text("클로즈 그립 랫 풀 다운")
+                    .font(.headline1())
+                    .foregroundColor(.label_900)
+                Spacer()
+                
+                Text("1450kg")
+                    .font(.headline1())
+                    .foregroundColor(.label_900)
+            }
+            
+            HStack {
+                Spacer()
+                Text("1세트")
+                    .font(.body2())
+                    .foregroundColor(.label_700)
+                
+                Text("40kg")
+                    .font(.body2())
+                    .foregroundColor(.label_700)
+                Image(systemName: "xmark")
+                    .font(.body2())
+                    .foregroundColor(.label_400)
+                Text("10회")
+                    .font(.body2())
+                    .foregroundColor(.label_700)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
-#Preview {
-    RecordDetailView()
+struct RecordDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordDetailView()
+    }
 }
