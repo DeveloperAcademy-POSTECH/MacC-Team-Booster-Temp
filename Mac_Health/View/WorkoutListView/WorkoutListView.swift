37c29a522efa184073a436957d43b38b18bf2331
songmoro <wlqgkr@naver.com>
2023-11-04 21:43:39 +0900

    feat: 운동 목록 뷰 init

---
+++ Mac_Health/View/WorkoutListView/WorkoutListView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutListView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/4/23.
+//
+
+import SwiftUI
+
+struct WorkoutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutListView()
+}
