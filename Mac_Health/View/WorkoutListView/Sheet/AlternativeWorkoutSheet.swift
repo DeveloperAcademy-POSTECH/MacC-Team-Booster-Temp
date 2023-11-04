09e7390e56ee1578ef59b28b06f9d83173c7d79b
songmoro <wlqgkr@naver.com>
2023-11-04 23:40:03 +0900

    feat: 대체 운동 시트 init

---
+++ Mac_Health/View/WorkoutListView/Sheet/AlternativeWorkoutSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AlternativeWorkoutSheet.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/4/23.
+//
+
+import SwiftUI
+
+struct AlternativeWorkoutSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlternativeWorkoutSheet()
+}
