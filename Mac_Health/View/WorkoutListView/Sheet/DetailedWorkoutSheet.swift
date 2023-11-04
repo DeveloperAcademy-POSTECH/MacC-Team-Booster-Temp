185e7dc3422e14acb4d7e8b144a77965e4253400
songmoro <wlqgkr@naver.com>
2023-11-04 23:04:35 +0900

    feat: 시트 사이즈 조정, 액션 버튼 추가

---
+++ Mac_Health/View/WorkoutListView/Sheet/DetailedWorkoutSheet.swift
@@ -0,0 +1,116 @@
+//
+//  DetailedWorkout.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/4/23.
+//
+
+import SwiftUI
+
+struct DetailedWorkoutSheet: View {
+    var body: some View {
+        ZStack {
+            Color.gray_800.ignoresSafeArea()
+            
+            VStack {
+                NavigationTitle
+                ScrollView {
+                    VStack {
+                        WorkoutCard
+                        WorkoutTip
+                        RelatedContent
+                    }
+                }
+            }
+        }
+    }
+    
+    var NavigationTitle: some View {
+        HStack {
+            Text("클로즈 그립 랫 풀 다운")
+                .font(.title1())
+                .foregroundColor(.label_900)
+            
+            Spacer()
+            
+            Button {
+                //                dismiss()
+            } label: {
+                Circle()
+                    .foregroundColor(.gray_600)
+                    .frame(width: UIScreen.getWidth(30), height: UIScreen.getHeight(30))
+                    .overlay {
+                        Image(systemName: "multiply")
+                            .foregroundColor(.label_700)
+                            .font(.headline1())
+                    }
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top)
+    }
+    
+    var WorkoutCard: some View {
+        VStack {
+            HStack {
+                Text("등")
+                    .foregroundColor(.label_700)
+                    .font(.body())
+                
+                Spacer()
+            }
+            
+            Image("Workout")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(220))
+        }
+        .padding(.horizontal)
+    }
+    
+    var WorkoutTip: some View {
+        VStack {
+            HStack {
+                Image("descriptionFace1")
+                    .resizable()
+                    .frame(width: UIScreen.getWidth(48), height: UIScreen.getHeight(48))
+                    .foregroundColor(.gray_600)
+                
+                Spacer()
+            }
+            .padding(.bottom)
+            
+            Text("고정축이 흔들리면 정확한 타겟이 불가능하기 때문에 꼭! 랫 풀 다운 할 때는 발꿈치를 들어서 무릎 패드와 다리 사이에 공간이 없도록 단단한 지지대를 만들어 주는 것이 굉장히 중요합니다.\n")
+                .multilineTextAlignment(.leading)
+                .lineSpacing(3)
+                .foregroundColor(.label_900)
+                .font(.headline1())
+        }
+        .padding(.horizontal)
+    }
+    
+    var RelatedContent: some View {
+        VStack {
+            HStack {
+                Text("관련 영상")
+                    .font(.title2())
+                    .foregroundColor(.label_900)
+                
+                Spacer()
+            }
+            
+            ScrollView(.horizontal) {
+                HStack{
+                    RelatedContentCard(videoNum: 0, contentURL: "")
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct DetailedWorkout_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedWorkoutSheet()
+    }
+}
