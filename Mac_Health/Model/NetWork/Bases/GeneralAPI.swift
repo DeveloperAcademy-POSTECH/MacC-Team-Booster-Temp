847549a73debb648b5bb543b74f7348ba5be3cb1
songmoro <wlqgkr@naver.com>
2023-11-08 17:04:47 +0900

    feat: api 메서드, 경로, 바디 수정

--- Mac_Health/Model/NetWork/Bases/GeneralAPI.swift
+++ Mac_Health/Model/NetWork/Bases/GeneralAPI.swift
@@ -36,13 +36,13 @@ enum GeneralAPI {
     
     // MARK: user-set-controller
     /// 세트 조정 - WorkoutOngoingView
-    case PatchUsersRoutinesExercisesSets(routineId: Int, exerciseId: Int, SetId: Int)
+    case PatchUsersRoutinesExercisesSets(routineId: Int, exerciseId: Int, setId: Int)
     
     /// 세트 완료 - WorkoutOngoingView
-    case PatchUsersRoutinesExercisesSetsFinish(routineId: Int, exerciseId: Int, SetId: Int)
+    case PatchUsersRoutinesExercisesSetsFinish(routineId: Int, exerciseId: Int, setId: Int)
     
     /// 세트 취소(안 씀) - WorkoutOngoingView
-    case PatchUsersRoutinesExercisesSetsCancle(routineId: Int, exerciseId: Int, SetId: Int)
+    case PatchUsersRoutinesExercisesSetsCancle(routineId: Int, exerciseId: Int, setId: Int)
     //:
     
     // MARK: routine-controller
@@ -65,66 +65,84 @@ extension GeneralAPI: TargetType {
     
     var path: String {
         switch self {
-        case .PostExercise(routineId: let routineId, exerciseId: let exerciseId):
+        case .PatchRoutinesExercisesSets(let routineId, let exerciseId):
             return "/routines/\(routineId)/exercises/\(exerciseId)/sets"
-        case .DeleteExercise(routineId: let routineId, exerciseId: let exerciseId):
+        case .DeleteRoutinesExercisesSets(let routineId, let exerciseId):
             return "/routines/\(routineId)/exercises/\(exerciseId)/sets"
-        case .PatchSet(routineId: let routineId, exerciseId: let exerciseId, setId: let setId, weight: _, reps: _):
-            return "/routines/\(routineId)/exercises/\(exerciseId)/sets/\(setId)"
-        case .PatchSetFinish(routineId: let routineId, exerciseId: let exerciseId, setId: let setId):
-            return "/routines/\(routineId)/exercises/\(exerciseId)/sets/\(setId)/finish"
-        case .GetSpecificExercise(routineId: let routineId, exerciseId: let exerciseId):
+        case .GetRoutinesExercises(let routineId, let exerciseId):
             return "/routines/\(routineId)/exercises/\(exerciseId)"
-        case .PatchRoutineFinish(routineId: let routineId):
+        case .PatchUsersRoutinesFinish(let routineId):
             return "/users/routines/\(routineId)/finish"
-        case .GetInfluencerRoutine(id: let id):
-            return "/useres/routines/\(id)"
-        case .GetInfluencerAllRoutine(id: let id):
+        case .GetUsersRoutines(let date):
+            return "/users/routines/?date=\(date)"
+        case .GetUsersRoutinesId(let id):
+            return "/users/routines/\(id)"
+        case .GetUsersInfluencersRoutines(let id):
             return "/users/influencers/\(id)/routines"
+        case .PatchUsersRoutinesExercisesSets(let routineId, let exerciseId, let setId):
+            return "/users/routines/\(routineId)/exercises/\(exerciseId)/sets/\(setId)"
+        case .PatchUsersRoutinesExercisesSetsFinish(let routineId, let exerciseId, let setId):
+            return "/users/routines/\(routineId)/exercises/\(exerciseId)/sets/\(setId)/finish"
+        case .PatchUsersRoutinesExercisesSetsCancle(let routineId, let exerciseId, let setId):
+            return "/users/routines/\(routineId)/exercises/\(exerciseId)/sets/\(setId)/cancle"
+        case .GetRoutines:
+            return "/routines"
+        case .GetInfluencersRoutines(let id):
+            return "/influencers/\(id)/routines"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .PostExercise:
-             return .post
-        case .DeleteExercise:
-            return .delete
-        case .PatchSet:
-            return .patch
-        case .PatchSetFinish:
-            return .patch
-        case .GetSpecificExercise:
-            return .get
-        case .PatchRoutineFinish:
-            return .patch
-        case .GetInfluencerRoutine:
-            return .get
-        case .GetInfluencerAllRoutine:
-            return .get
+        case .PatchRoutinesExercisesSets: return .patch
+        case .DeleteRoutinesExercisesSets: return .delete
+        case .GetRoutinesExercises: return .get
+        case .PatchUsersRoutinesFinish: return .patch
+        case .GetUsersRoutines: return .get
+        case .GetUsersRoutinesId: return .get
+        case .GetUsersInfluencersRoutines: return .get
+        case .PatchUsersRoutinesExercisesSets: return .patch
+        case .PatchUsersRoutinesExercisesSetsFinish: return .patch
+        case .PatchUsersRoutinesExercisesSetsCancle: return .patch
+        case .GetRoutines: return .get
+        case .GetInfluencersRoutines: return .get
         }
     }
     
     var task: Moya.Task {
         switch self {
-        case .PostExercise(routineId: _, exerciseId: _):
-            return .requestPlain
-        case .DeleteExercise(routineId: _, exerciseId: _):
-            return .requestPlain
-        //TODO: 여기 부분 다시 처리해줘야함
-        case .PatchSet(routineId: _, exerciseId: _, setId: _, weight: let weight, reps: let reps):
-            return .requestJSONEncodable(RequestPatchSet(weight: weight, reps: reps))
-        case .PatchSetFinish(routineId: _, exerciseId: _, setId: _):
-            return .requestPlain
-        case .GetSpecificExercise(routineId: _, exerciseId: _):
-            return .requestPlain
-        case .PatchRoutineFinish(routineId: _):
-            return .requestPlain
-        case .GetInfluencerRoutine(id: _):
-            return .requestPlain
-        case .GetInfluencerAllRoutine(id: _):
-            return .requestPlain
+        case .PatchRoutinesExercisesSets: return .requestPlain
+        case .DeleteRoutinesExercisesSets: return .requestPlain
+        case .GetRoutinesExercises: return .requestPlain
+        case .PatchUsersRoutinesFinish: return .requestPlain
+        case .GetUsersRoutines: return .requestPlain
+        case .GetUsersRoutinesId: return .requestPlain
+        case .GetUsersInfluencersRoutines: return .requestPlain
+        case .PatchUsersRoutinesExercisesSets: return .requestPlain
+        case .PatchUsersRoutinesExercisesSetsFinish: return .requestPlain
+        case .PatchUsersRoutinesExercisesSetsCancle: return .requestPlain
+        case .GetRoutines: return .requestPlain
+        case .GetInfluencersRoutines: return .requestPlain
         }
+//        switch self {
+//        case .PostExercise(routineId: _, exerciseId: _):
+//            return .requestPlain
+//        case .DeleteExercise(routineId: _, exerciseId: _):
+//            return .requestPlain
+//        //TODO: 여기 부분 다시 처리해줘야함
+//        case .PatchSet(routineId: _, exerciseId: _, setId: _, weight: let weight, reps: let reps):
+//            return .requestJSONEncodable(RequestPatchSet(weight: weight, reps: reps))
+//        case .PatchSetFinish(routineId: _, exerciseId: _, setId: _):
+//            return .requestPlain
+//        case .GetSpecificExercise(routineId: _, exerciseId: _):
+//            return .requestPlain
+//        case .PatchRoutineFinish(routineId: _):
+//            return .requestPlain
+//        case .GetInfluencerRoutine(id: _):
+//            return .requestPlain
+//        case .GetInfluencerAllRoutine(id: _):
+//            return .requestPlain
+//        }
     }
     
     var headers: [String : String]? {
