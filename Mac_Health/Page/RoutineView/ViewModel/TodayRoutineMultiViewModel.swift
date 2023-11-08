a6fea02abeda89eb26b62ebf16d6456c90c3f35c
songmoro <wlqgkr@naver.com>
2023-11-09 01:04:21 +0900

    feat: TodayRoutineMultiView, ViewModel 데이터 연결

--- Mac_Health/Page/RoutineView/ViewModel/TodayRoutineMultiViewModel.swift
+++ Mac_Health/Page/RoutineView/ViewModel/TodayRoutineMultiViewModel.swift
@@ -6,7 +6,34 @@
 //
 
 import SwiftUI
+import Moya
 
 class TodayRoutineMultiViewModel: ObservableObject {
+    let provider = MoyaProvider<GeneralAPI>()
+    let date = "2023-10-30"
     
+    @Published var routines = ResponseGetUsersRoutines(routines: [])
+    
+    init() {
+        self.fetchRoutines()
+    }
+    
+    func fetchRoutines() {
+        provider.request(.GetUsersRoutines(date: date)) { result in
+            switch result {
+            case .success(let resp):
+                switch resp.statusCode {
+                case 200:
+                    let resultData = try! JSONDecoder().decode([InfluencerRoutine].self, from: resp.data)
+                    self.routines = ResponseGetUsersRoutines(routines: resultData)
+                    print(self.routines)
+                default:
+                    print("Status Code: \(resp.statusCode)")
+                    break
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
