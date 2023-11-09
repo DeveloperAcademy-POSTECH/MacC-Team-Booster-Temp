4ff68c2b4f572ff0acb0b3d3fdc52ee4a6548f7b
CHUNG Hoiseong <pmchung423@gmail.com>
2023-11-09 18:17:34 +0900

    reflecting design

--- Mac_Health/Page/SearchView/View/SubscribeView.swift
+++ Mac_Health/Page/SearchView/View/SubscribeView.swift
@@ -39,29 +39,30 @@ struct SubscribeView: View {
             Color.gray_900.ignoresSafeArea()
             ScrollView {
                 VStack{
-                //구독 페이지 설명
+                    //구독 페이지 설명
                     IntroPage
-                //구독 버튼
-                subscribeButton
-                //인플루언서 설명
+                    //구독 버튼
+                    subscribeButton
+                    //인플루언서 설명
                     introInfluencer
-                //루틴 미리보기
-                RoutinePreview(previewRoutine: previewRoutine[0])
-                    .padding(.horizontal, 10)
-            }
+                    bodyInformation
+                    //루틴 미리보기
+                    RoutinePreview()
+                        .padding(.horizontal)
+                }
                 .background(GeometryReader {
                     return Color.clear.preference(key: ViewOffsetKey.self, value: -$0.frame(in: .named("scroll")).origin.y)
                 })
                 .onPreferenceChange(ViewOffsetKey.self) { offset in
-                                withAnimation {
-                                    if offset > UIScreen.getHeight(422) {
-                                        showTab = true
-                                    } else  {
-                                        showTab = false
-                                    }
-                                }
-                                scrollOffset = offset
-                            }
+                    withAnimation {
+                        if offset > UIScreen.getHeight(422) {
+                            showTab = true
+                        } else  {
+                            showTab = false
+                        }
+                    }
+                    scrollOffset = offset
+                }
             }
             .coordinateSpace(name: "scroll")
             .overlay(
@@ -95,163 +96,161 @@ struct SubscribeView: View {
             }
             .tabViewStyle(.page)
             topInfluencerDescription
-                
+            
         }
     }
-        
-        var BackButton: some View {
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "chevron.left")
-                    .foregroundColor(.label_900)
-                    .font(.body())
-            }
+    
+    var BackButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.label_900)
+                .font(.body())
         }
+    }
     
     @ViewBuilder
     var introInfluencer: some View {
-        VStack(alignment: .leading){
-            HStack {
-                Text("인사말")
+        VStack{
+            VStack(alignment: .leading){
+                HStack {
+                    Text("인사말")
+                        .foregroundColor(.label_900)
+                        .font(.headline1())
+                        .padding(.top, 20)
+                    Spacer()
+                }
+                Text(introduce)
+                    .foregroundColor(.label_800)
+                    .font(.body)
+                    .padding(.vertical, 10)
+                Text("수상경력")
                     .foregroundColor(.label_900)
                     .font(.headline1())
-                .padding(.top, 20)
-                Spacer()
+                    .padding(.top, 20)
             }
-            Text(introduce)
-                .foregroundColor(.label_800)
-                .font(.body)
-                .padding(.vertical, 10)
-            Text("수상경력")
-                .foregroundColor(.label_900)
-                .font(.headline1())
-                .padding(.top, 20)
-        }
-        .padding(.leading)
-        .frame(maxWidth: .infinity)
-        
-    if seeMore == false {
-        ZStack(alignment: .top){
-            VStack(alignment: .leading){
-                ForEach(0..<award.count, id: \.self) { index in
-                    HStack{
-                        VStack{
-                        Text("•")
-                                .font(.body)
-                            .foregroundColor(.label_800)
-                        Spacer()
-                    }
-                        VStack(alignment: .leading){
-                            HStack{
-                                Text(award[index])
+            .padding(.leading)
+            .frame(maxWidth: .infinity)
+            //MARK: 잼이 지우지 말래요.
+//            if seeMore == false {
+//                ZStack(alignment: .top){
+//                    VStack(alignment: .leading){
+//                        ForEach(0..<award.count, id: \.self) { index in
+//                            HStack{
+//                                VStack{
+//                                    Text("•")
+//                                        .font(.body)
+//                                        .foregroundColor(.label_800)
+//                                    Spacer()
+//                                }
+//                                VStack(alignment: .leading){
+//                                    HStack{
+//                                        Text(award[index])
+//                                            .font(.body)
+//                                            .foregroundColor(.label_800)
+//                                        Spacer()
+//                                    }
+//                                    Spacer()
+//                                }
+//                            }
+//                            .padding(.leading, 10)
+//                        }
+//                    }
+//                    .mask(alignment: .top){
+//                        Rectangle()
+//                            .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(130))
+//                    }
+//                    .padding()
+//                    LinearGradient(colors: [.clear, .clear, .gray_900.opacity(0.7), .gray_900], startPoint: .top, endPoint: .bottom)
+//                    Button {
+//                        seeMore = true
+//                    } label: {
+//                        VStack{
+//                            Spacer()
+//                                .frame(height: UIScreen.getHeight(170))
+//                            Text("더보기")
+//                                .font(.headline1())
+//                                .foregroundColor(Color.label_900)
+//                        }
+//                    }
+//                    
+//                }
+//                .frame(width: UIScreen.getWidth(384), height: UIScreen.getHeight(150))
+//                .padding(.top, 40)
+//            }
+//            else {
+                VStack(alignment: .leading){
+                    ForEach(0..<award.count, id: \.self) { index in
+                        HStack {
+                            VStack {
+                                Text("•")
                                     .font(.body)
                                     .foregroundColor(.label_800)
                                 Spacer()
                             }
-                            Spacer()
+                            VStack(alignment: .leading){
+                                HStack {
+                                    Text(award[index])
+                                        .font(.body)
+                                        .foregroundColor(.label_800)
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
                         }
-                    }
-                    .padding(.leading, 10)
-                }
-            }
-            .mask(
-            Rectangle()
-                .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(230)))
-            .padding()
-            LinearGradient(colors: [.clear, .clear, .gray_900.opacity(0.7), .gray_900], startPoint: .top, endPoint: .bottom)
-            Button {
-                seeMore = true
-            } label: {
-                VStack{
-                    Spacer()
-                        .frame(height: UIScreen.getHeight(170))
-                    Text("더보기")
-                        .font(.headline1())
-                        .foregroundColor(Color.label_900)
-                }
-            }
-            
-        }
-        .frame(width: UIScreen.getWidth(384), height: UIScreen.getHeight(150))
-    }
-    else {
-        VStack(alignment: .leading){
-            ForEach(0..<award.count, id: \.self) { index in
-                HStack {
-                    VStack {
-                    Text("•")
-                            .font(.body)
-                        .foregroundColor(.label_800)
-                    Spacer()
-                }
-                    VStack(alignment: .leading){
-                        HStack {
-                            Text(award[index])
-                                .font(.body)
-                                .foregroundColor(.label_800)
-                            Spacer()
-                        }
-                        Spacer()
+                        .padding(.leading, 10)
                     }
                 }
-                .padding(.leading, 10)
-            }
+                .padding()
+//            }
         }
-        .padding()
     }
-    }
-        
-        
     
+    
+    @ViewBuilder
     var subscribeButton: some View {
-        Button {
-            self.subscribingSheet = true
-            self.subscribed.toggle()
-        } label: {
-            FloatingButton(backgroundColor: subscribed ? .green_main : .gray_900) { subscribed ? Text("구독")
-                    .foregroundColor(.gray_900)
-                    .font(.button1())
-                :
-                Text("구독 취소")
-                    .foregroundColor(.red_main)
+       if loggedIn {
+            Button {
+                self.subscribingSheet = true
+                self.subscribed.toggle()
+            } label: {
+                FloatingButton(backgroundColor: subscribed ? .green_main : .gray_900) { subscribed ? Text("구독")
+                        .foregroundColor(.gray_900)
+                        .font(.button1())
+                    :
+                    Text("구독 취소")
+                        .foregroundColor(.red_main)
+                        .font(.button1())
+                }
+                
+            }
+            .alert(isPresented: $subscribingSheet) {
+                subscribed ?
+                Alert(
+                    title: Text("구독이 취소되었습니다."),
+                    message: Text(""),
+                    dismissButton: .destructive(Text("확인"),
+                                                action: {
+                                                    //구독 취소
+                                                    
+                                                })
+                ) :
+                Alert(
+                    title: Text("구독이 완료되었습니다."),
+                    message: Text(""),
+                    dismissButton: .destructive(Text("확인"),
+                                                action: {
+                                                    //구독 완료
+                                                    
+                                                })
+                )
+            }
+        } else {
+            FloatingButton(backgroundColor: .gray_600) {Text("둘러보기 중")
+                    .foregroundColor(.label_400)
                     .font(.button1())
             }
-
-        }
-        .alert(isPresented: $subscribingSheet) {
-            loggedIn ?
-            subscribed ?
-            Alert(
-                title: Text("구독이 취소되었습니다."),
-                message: Text(""),
-                dismissButton: .destructive(Text("확인"),
-                                        action: {
-                                            //구독 취소
-                                            
-                                })
-            ) :
-            Alert(
-                title: Text("구독이 완료되었습니다."),
-                message: Text(""),
-                dismissButton: .destructive(Text("확인"),
-                                        action: {
-                                            //구독 완료
-                                            
-                                })
-            )
-            :
-            Alert(
-                title: Text("로그인이 필요합니다."),
-                message: Text(""),
-                primaryButton: .destructive(Text("확인"),
-                                        action: {
-                                            //로그인 뷰 이동
-                                            
-                                }),
-                                secondaryButton: .cancel(Text("취소"))
-            )
-            
         }
         
     }
@@ -282,7 +281,27 @@ struct SubscribeView: View {
             }
         }
         .frame(height: UIScreen.getHeight(358))
-        
+    }
+    
+    var bodyInformation: some View {
+        HStack{
+            VStack(alignment: .leading){
+                Text("신체정보")
+                    .foregroundColor(.label_900)
+                    .font(.headline1())
+                    .padding(.top, 20)
+                Text("키: 173cm")
+                    .foregroundColor(.label_800)
+                    .font(.body)
+                    .padding(.vertical, 10)
+                Text("몸무게: 80kg")
+                    .foregroundColor(.label_800)
+                    .font(.body)
+                    .padding(.vertical, 10)
+            }
+         Spacer()
+        }
+        .padding(.horizontal)
     }
     
     var topBackground: some View {
@@ -296,7 +315,7 @@ struct SubscribeView: View {
             }
         }
         .frame(height: UIScreen.getHeight(358))
-
+        
     }
     
 }
@@ -312,10 +331,7 @@ struct ViewOffsetKey: PreferenceKey {
 struct RoutinePreview: View {
     //날짜 변환
     var date: String = "2023년 10월 15일"
-    var part: String = "등/가슴"
     var firstPart = "등"
-    var secondPart = "가슴"
-    let previewRoutine: PreviewRoutine
     
     var body: some View {
         ZStack{
@@ -323,9 +339,13 @@ struct RoutinePreview: View {
             VStack {
                 HStack{
                     VStack(alignment: .leading){
-                        Text("루틴 미리보기")
+                        Divider()
+                            .foregroundColor(.fill_1)
+                            .padding(.top)
+                        Text("운동일지 미리보기")
                             .font(.title2)
                             .foregroundColor(.label_900)
+                            .padding(.top, 30)
                         HStack{
                             Text(date)
                                 .font(.headline2())
@@ -333,26 +353,29 @@ struct RoutinePreview: View {
                             Rectangle()
                                 .frame(width:1, height: 10)
                                 .foregroundColor(.label_400)
-                            Text(part)
+                            Text("등/복근")
                                 .font(.headline2())
                                 .foregroundColor(.label_700)
                             
                         }
                     }
-                    .padding(20)
+                    .padding(.vertical, 20)
                     Spacer()
                 }
                 HStack{
                     Text(firstPart)
                         .font(.headline1())
                         .foregroundColor(.label_900)
-                        .padding(20)
+                        .padding(.vertical, 20)
                     Spacer()
                 }
                 //운동 루틴
-                WorkoutExplain
+                WorkoutExplain(ImageName: "CloseGripLatPullDown", WorkoutName: "하이퍼 익스텐션", SetCount: "3세트", part: "등")
                 //첫 운동 팁 설명
                 //ZStack{
+                
+                //                }
+                WorkoutExplain(ImageName: "CloseGripLatPullDown", WorkoutName: "덤벨 풀 오버", SetCount: "3세트", part: "등")
                 HStack(alignment: .top){
                     Image("descriptionFace1")
                         .resizable()
@@ -364,57 +387,49 @@ struct RoutinePreview: View {
                             .font(.body2())
                             .foregroundColor(.label_500)
                             .padding(.bottom, 5)
-                        
-                        Text(previewRoutine.exerciseTip!)
+                        Text("견갑의 가동 범위를 늘려 주기 좋은 풀 오버입니다. 골반을 지면 가까이 붙여주시고 팔꿈치는 하늘을 향하게 해주세요. 그리고 이두가 귀에 닿을 정도로 가까이해서 내려가주세요.")
                             .font(.body)
                             .foregroundColor(.label_900)
                     }
-                    .padding(.vertical, 10)
-                }
                     .padding(10)
-                    .background{
-                        RoundedRectangle(cornerRadius: 8)
-                            .foregroundColor(.gray_700)
-                    }
-                    .padding(20)
-//                }
-                WorkoutExplain
-                //부위별 다른지 체크
-                HStack{
-                    Text(secondPart)
-                        .font(.headline1())
-                        .foregroundColor(.label_900)
-                        .padding(20)
-                    Spacer()
                 }
-                WorkoutExplain
-                WorkoutExplain
+                .padding(10)
+                .background{
+                    RoundedRectangle(cornerRadius: 8)
+                        .foregroundColor(.gray_700)
+                }
+                .padding(.vertical, 20)
+                WorkoutExplain(ImageName: "CloseGripLatPullDown", WorkoutName: "바벨 로우(머신)", SetCount: "3세트", part: "등")
                 
             }
             .padding(.bottom, 30)
             VStack{
                 Spacer()
-                Image("previewGradient")
-                    .resizable()
-                    .scaledToFit()
+                LinearGradient(colors: [.clear, .clear, .clear, .clear, .gray_900.opacity(0.5), .gray_900], startPoint: .top, endPoint: .bottom)
             }
         }
         
     }
     
-    @ViewBuilder
-    var WorkoutExplain: some View {
+}
+
+struct WorkoutExplain: View {
+    let ImageName: String
+    let WorkoutName: String
+    let SetCount: String
+    let part: String
+    
+    var body: some View {
         HStack{
-            Image("CloseGripLatPullDown")
+            Image(ImageName)
                 .resizable()
                 .frame(width: UIScreen.getWidth(64), height: UIScreen.getHeight(64))
-                .padding(.leading, 20)
             VStack(alignment: .leading){
-                Text("클로즈 그립 랫 풀 다운")
+                Text(WorkoutName)
                     .font(.headline1())
                     .foregroundColor(.label_900)
                 HStack{
-                    Text("3세트")
+                    Text(SetCount)
                         .font(.headline2())
                         .foregroundColor(.label_700)
                     Rectangle()
@@ -426,10 +441,10 @@ struct RoutinePreview: View {
                     
                 }
             }
-            .padding(.leading, 10)
-
+            
             Spacer()
         }
+        .frame(maxWidth: .infinity)
     }
 }
 
