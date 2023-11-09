d6cb5cf00884da338fa7ec190cc4df016a3232c2
songmoro <wlqgkr@naver.com>
2023-11-09 16:23:13 +0900

    feat: WorkoutOngoingVM  api 호출 함수 변경

--- Mac_Health/Page/RoutineView/ViewModel/WorkoutOngoingVM.swift
+++ Mac_Health/Page/RoutineView/ViewModel/WorkoutOngoingVM.swift
@@ -6,10 +6,8 @@
 //
 
 import SwiftUI
-import Moya
 
 class WorkoutOngoingVM: ObservableObject {
-    let provider = MoyaProvider<GeneralAPI>()
     let routineId = 1
     let exerciseId = 1
     let setId = 1
@@ -24,18 +22,11 @@ class WorkoutOngoingVM: ObservableObject {
     
     func fetchRoutine() {
         // TODO: 여러 번(= 8번) init 되는 거 수정
-        provider.request(.GetRoutinesExercises(routineId: routineId, exerciseId: exerciseId)) { result in
-            switch result {
-            case .success(let resp):
-                switch resp.statusCode {
-                case 200:
-                    let resultData = try! JSONDecoder().decode(ResponseGetRoutinesExercises.self, from: resp.data)
-                    self.routine = resultData
-                    print(self.routine)
-                default:
-                    print("Status Code: \(resp.statusCode)")
-                    break
-                }
+        GeneralAPIManger.request(for: .GetRoutinesExercises(routineId: routineId, exerciseId: exerciseId), type: ResponseGetRoutinesExercises.self) {
+            switch $0 {
+            case .success(let routine):
+                self.routine = routine
+                print(self.routine)
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -43,18 +34,11 @@ class WorkoutOngoingVM: ObservableObject {
     }
     
     func postIncreaseSet() {
-        provider.request(.PostRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId)) { result in
-            switch result {
-            case .success(let resp):
-                switch resp.statusCode {
-                case 200:
-                    let resultData = try! JSONDecoder().decode(ResponsePostRoutinesExercisesSets.self, from: resp.data)
-                    self.routine.sets = resultData.sets
-                    print(self.routine)
-                default:
-                    print("Status Code: \(resp.statusCode)")
-                    break
-                }
+        GeneralAPIManger.request(for: .PostRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId), type: ResponsePostRoutinesExercisesSets.self) {
+            switch $0 {
+            case .success(let set):
+                self.routine.sets = set.sets
+                print(self.routine)
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -62,18 +46,11 @@ class WorkoutOngoingVM: ObservableObject {
     }
     
     func deleteDecreaseSet() {
-        provider.request(.DeleteRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId)) { result in
-            switch result {
-            case .success(let resp):
-                switch resp.statusCode {
-                case 200:
-                    let resultData = try! JSONDecoder().decode(ResponseDeleteRoutinesExercisesSets.self, from: resp.data)
-                    self.routine.sets = resultData.sets
-                    print(self.routine)
-                default:
-                    print("Status Code: \(resp.statusCode)")
-                    break
-                }
+        GeneralAPIManger.request(for: .DeleteRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId), type: ResponseDeleteRoutinesExercisesSets.self) {
+            switch $0 {
+            case .success(let set):
+                self.routine.sets = set.sets
+                print(self.routine)
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -81,20 +58,13 @@ class WorkoutOngoingVM: ObservableObject {
     }
     
     func patchSetFinish() {
-        provider.request(.PatchUsersRoutinesExercisesSetsFinish(routineId: routineId, exerciseId: exerciseId, setId: setId)) { result in
-            switch result {
-            case .success(let resp):
-                switch resp.statusCode {
-                case 200:
-                    let resultData = try! JSONDecoder().decode(ResponsePatchUsersRoutinesExercisesSetsFinish.self, from: resp.data)
-                    self.routine.sets[self.setId].weight = resultData.weight
-                    self.routine.sets[self.setId].reps = resultData.reps
-                    self.routine.sets[self.setId].isDone = resultData.isDone
-                    print(self.routine)
-                default:
-                    print("Status Code: \(resp.statusCode)")
-                    break
-                }
+        GeneralAPIManger.request(for: .PatchUsersRoutinesExercisesSetsFinish(routineId: routineId, exerciseId: exerciseId, setId: setId), type: ResponsePatchUsersRoutinesExercisesSetsFinish.self) {
+            switch $0 {
+            case .success(let set):
+                self.routine.sets[self.setId].weight = set.weight
+                self.routine.sets[self.setId].reps = set.reps
+                self.routine.sets[self.setId].isDone = set.isDone
+                print(self.routine)
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -102,20 +72,13 @@ class WorkoutOngoingVM: ObservableObject {
     }
     
     func patchSetModification(weight: Int, reps: Int) {
-        provider.request(.PatchUsersRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId, setId: setId, weight: weight, reps: reps)) { result in
-            switch result {
-            case .success(let resp):
-                switch resp.statusCode {
-                case 200:
-                    let resultData = try! JSONDecoder().decode(ResponsePatchUsersRoutinesExercisesSets.self, from: resp.data)
-                    self.routine.sets[self.setId].weight = resultData.weight
-                    self.routine.sets[self.setId].reps = resultData.reps
-                    self.routine.sets[self.setId].isDone = resultData.isDone
-                    print(self.routine)
-                default:
-                    print("Status Code: \(resp.statusCode)")
-                    break
-                }
+        GeneralAPIManger.request(for: .PatchUsersRoutinesExercisesSets(routineId: routineId, exerciseId: exerciseId, setId: setId, weight: weight, reps: reps), type: ResponsePatchUsersRoutinesExercisesSets.self) {
+            switch $0 {
+            case .success(let set):
+                self.routine.sets[self.setId].weight = set.weight
+                self.routine.sets[self.setId].reps = set.reps
+                self.routine.sets[self.setId].isDone = set.isDone
+                print(self.routine)
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -123,73 +86,73 @@ class WorkoutOngoingVM: ObservableObject {
     }
     
     
-//    let workoutModel: WorkoutModel
-//    @Published var isRoutineSequenceShow = false
-//    @Published var isAlternativeWorkoutShow = false
-//    @Published var isWorkoutFinishAlertShow = false
-//    @Published var isWorkoutTipShow = false
-//    @Published var workoutSet: [WorkoutSetModel] = []
-//    @Published var currentSet = 1
+    //    let workoutModel: WorkoutModel
+    //    @Published var isRoutineSequenceShow = false
+    //    @Published var isAlternativeWorkoutShow = false
+    //    @Published var isWorkoutFinishAlertShow = false
+    //    @Published var isWorkoutTipShow = false
+    //    @Published var workoutSet: [WorkoutSetModel] = []
+    //    @Published var currentSet = 1
     
-//    init() {
-//        workoutModel = WorkoutModel(workoutDate: "10월 5일(일)", influencerName: "정회승", workoutName: "클로즈 그립 랫 풀 다운", workoutSet: 4, workoutKgs: [0, 0, 0, 0], workoutReps: [10, 10, 15, 15], alternativeWorkout: ["클로즈 그립 랫 풀 다운", "클로즈 그립 랫 풀 다운", "클로즈 그립 랫 풀 다운"], relatedContentURL: [ "http://www.youtube.com"], workoutTip: "고정축이 흔들리면 정확한 타겟이 불가능하기 때문에 꼭! 랫 풀 다운 할 때는 발꿈치를 들어서 무릎 패드와 다리 사이에 공간이 없도록 단단한 지지대를 만들어 주는 것이 굉장히 중요합니다.")
-//        
-//        for i in 0..<workoutModel.workoutSet {
-//            workoutSet.append(WorkoutSetModel(index: i + 1, kg: workoutModel.workoutKgs[i], repetition: workoutModel.workoutReps[i], isFinish: false))
-//        }
-//    }
-//    
-//    func decreaseWorkoutSet() {
-//        if workoutSet.count > 1 {
-//            if workoutSet.count == currentSet {
-//                currentSet -= 1
-//            }
-//            workoutSet.removeLast()
-//        }
-//    }
-//    
-//    func increaseWorkoutSet() {
-//        if workoutSet.count < 10 {
-//            workoutSet.append(WorkoutSetModel(index: workoutSet.count + 1, kg: 0, repetition: 0, isFinish: false))
-//        }
-//    }
-//    
-//    func controlRepetition() {
-//        if workoutSet.count == currentSet {
-//            workoutSet[currentSet - 1].isFinish = true
-//            showWorkoutFinishAlert()
-//        }
-//        else {
-//            workoutSet[currentSet - 1].isFinish = true
-//            currentSet += 1
-//        }
-//    }
-//    
-//    func showAlternativeWorkout() {
-//        isAlternativeWorkoutShow = true
-//    }
-//    
-//    func dismissAlternativeWorkOut() {
-//        isAlternativeWorkoutShow = false
-//    }
-//    
-//    func showWorkoutFinishAlert() {
-//        isWorkoutFinishAlertShow = true
-//    }
-//    
-//    func showRoutineSequenceShow() {
-//        isRoutineSequenceShow = true
-//    }
-//    
-//    func dismissRoutineSequenceShow() {
-//        isRoutineSequenceShow = false
-//    }
-//    
-//    func showWorkoutTip() {
-//        isWorkoutTipShow = true
-//    }
-//    
-//    func dismissWorkoutTip() {
-//        isWorkoutTipShow = false
-//    }
+    //    init() {
+    //        workoutModel = WorkoutModel(workoutDate: "10월 5일(일)", influencerName: "정회승", workoutName: "클로즈 그립 랫 풀 다운", workoutSet: 4, workoutKgs: [0, 0, 0, 0], workoutReps: [10, 10, 15, 15], alternativeWorkout: ["클로즈 그립 랫 풀 다운", "클로즈 그립 랫 풀 다운", "클로즈 그립 랫 풀 다운"], relatedContentURL: [ "http://www.youtube.com"], workoutTip: "고정축이 흔들리면 정확한 타겟이 불가능하기 때문에 꼭! 랫 풀 다운 할 때는 발꿈치를 들어서 무릎 패드와 다리 사이에 공간이 없도록 단단한 지지대를 만들어 주는 것이 굉장히 중요합니다.")
+    //
+    //        for i in 0..<workoutModel.workoutSet {
+    //            workoutSet.append(WorkoutSetModel(index: i + 1, kg: workoutModel.workoutKgs[i], repetition: workoutModel.workoutReps[i], isFinish: false))
+    //        }
+    //    }
+    //
+    //    func decreaseWorkoutSet() {
+    //        if workoutSet.count > 1 {
+    //            if workoutSet.count == currentSet {
+    //                currentSet -= 1
+    //            }
+    //            workoutSet.removeLast()
+    //        }
+    //    }
+    //
+    //    func increaseWorkoutSet() {
+    //        if workoutSet.count < 10 {
+    //            workoutSet.append(WorkoutSetModel(index: workoutSet.count + 1, kg: 0, repetition: 0, isFinish: false))
+    //        }
+    //    }
+    //
+    //    func controlRepetition() {
+    //        if workoutSet.count == currentSet {
+    //            workoutSet[currentSet - 1].isFinish = true
+    //            showWorkoutFinishAlert()
+    //        }
+    //        else {
+    //            workoutSet[currentSet - 1].isFinish = true
+    //            currentSet += 1
+    //        }
+    //    }
+    //
+    //    func showAlternativeWorkout() {
+    //        isAlternativeWorkoutShow = true
+    //    }
+    //
+    //    func dismissAlternativeWorkOut() {
+    //        isAlternativeWorkoutShow = false
+    //    }
+    //
+    //    func showWorkoutFinishAlert() {
+    //        isWorkoutFinishAlertShow = true
+    //    }
+    //
+    //    func showRoutineSequenceShow() {
+    //        isRoutineSequenceShow = true
+    //    }
+    //
+    //    func dismissRoutineSequenceShow() {
+    //        isRoutineSequenceShow = false
+    //    }
+    //
+    //    func showWorkoutTip() {
+    //        isWorkoutTipShow = true
+    //    }
+    //
+    //    func dismissWorkoutTip() {
+    //        isWorkoutTipShow = false
+    //    }
 }
