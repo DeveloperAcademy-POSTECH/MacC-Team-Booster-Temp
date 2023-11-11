74c0dc412dd25ae8f1c9c160654effa3bb2693f0
songmoro <wlqgkr@naver.com>
2023-11-12 01:17:25 +0900

    feat: MultiRoutine 관련 코드 리팩토링

--- Mac_Health/Common/Component/SubscribedInfluencerCard.swift
+++ Mac_Health/Common/Component/SubscribedInfluencerCard.swift
@@ -11,57 +11,54 @@ struct SubscribedInfluencerCard: View {
     let routine: InfluencerRoutine
     
     var body: some View {
-        ZStack{
-            Rectangle()
-                .foregroundColor(.gray_700)
-            HStack{
-                VStack(alignment: .leading){
-                    Text("\(routine.name)의 \(routine.routineName)")
-                        .font(.headline1())
-                        .foregroundColor(.label_900)
-                        .padding(.leading, 20)
-                        .padding(.top, 24)
-                    
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(.gray_700)
+            .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(156))
+            .overlay {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(routine.name)의 \(routine.routineName)")
+                            .font(.headline1())
+                            .foregroundColor(.label_900)
+                            .padding(.leading, 20)
+                            .padding(.top, 24)
+                        Spacer()
+                        HStack {
+                            if routine.numberOfExercise == 0 {
+                                Text("휴식")
+                                    .padding(.leading, 20)
+                            }
+                            else {
+                                Text(routine.part)
+                                    .padding(.leading, 20)
+                                Text("|")
+                                    .foregroundColor(.label_400)
+                                Text("\(routine.numberOfExercise)개")
+                            }
+                        }
+                        .foregroundColor(.label_700)
+                        .font(.headline2())
+                        .padding(.bottom, 24)
+                    }
+                    .frame(height: UIScreen.getHeight(156))
                     Spacer()
-                    HStack{
-                        if routine.numberOfExercise == 0 {
-                            Text("휴식")
-                                .font(.headline2())
-                                .foregroundColor(.label_700)
-                                .padding(.leading, 20)
-                                .padding(.bottom, 24)
-                        }
-                        else {
-                            Text(routine.part)
-                                .font(.headline2())
-                                .foregroundColor(.label_700)
-                                .padding(.leading, 20)
-                                .padding(.bottom, 24)
-                            Text("|")
-                                .font(.headline2())
-                                .foregroundColor(.label_700)
-                                .padding(.bottom, 24)
-                            Text("\(routine.numberOfExercise)개")
-                                .font(.headline2())
-                                .foregroundColor(.label_700)
-                                .padding(.bottom, 24)
-                        }
+                    AsyncImage(url: URL(string: routine.influencerProfileImageUrl)) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: UIScreen.getWidth(156))
+                    } placeholder: {
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(50)
                     }
                 }
-                .frame(height: UIScreen.getHeight(156))
-                Spacer()
-                AsyncImage(url: URL(string: routine.influencerProfileImageUrl)) {
-                    $0.image?
-                        .scaledToFill()
-                        .frame(width: UIScreen.getWidth(156))
-                }
             }
-        }
-        .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(156))
-        .mask(
-            RoundedRectangle(cornerRadius: 8)
-                .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(156))
-        )
+            .mask {
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: UIScreen.getWidth(350), height: UIScreen.getHeight(156))
+            }
     }
 }
 
@@ -70,4 +67,3 @@ struct SubscribedInfluencerCard_Previews: PreviewProvider {
         SubscribedInfluencerCard(routine: InfluencerRoutine(routineId: 1, part: "등, 이두", date: "2023-10-30", numberOfExercise: 6, burnedKCalories: 300, requiredMinutes: 300, comment: "오늘도 화이팅!", name: "정회승", routineName: "올라잇", influencerProfileImageUrl: "", influencerId: 5))
     }
 }
-
