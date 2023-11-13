da05e5e2f24a049eda82fe21149a20e4c2b410ad
songmoro <wlqgkr@naver.com>
2023-11-14 01:40:49 +0900

    feat: 대체 운동 시트 뷰모델 추가

---
+++ Mac_Health/Page/RoutineView/ViewModel/AlternativeWorkoutSheetViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AlternativeWorkoutSheetViewModel.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/14/23.
+//
+
+import SwiftUI
+
+struct AlternativeWorkoutSheetViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlternativeWorkoutSheetViewModel()
+}
