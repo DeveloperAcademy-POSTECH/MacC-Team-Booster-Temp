95a0e3787b2df1fe66346cb7ff8e5a958d324444
songmoro <wlqgkr@naver.com>
2023-11-15 06:17:26 +0900

    feat: 전체 루틴 뷰 모델 함수 추가

--- Mac_Health/Page/RoutineView/ViewModel/WholeRoutineViewModel.swift
+++ Mac_Health/Page/RoutineView/ViewModel/WholeRoutineViewModel.swift
@@ -8,5 +8,31 @@
 import SwiftUI
 
 class WholeRoutineViewModel: ObservableObject {
+    // TODO: 루틴 모델 변경
+    /// 전체 루틴 목록
+    @Published var routines: [String] = []
     
+    // TODO: 루틴 부위들 추가
+    /// 선택된 부위
+    @Published var selectedPart: String = ""
+    
+    /// 전체 루틴 조회 함수
+    func fetchRoutines(influencerId: Int) {
+        // TODO: 루틴 목록 조회
+    }
+    
+    /// 루틴 월 별 분류 함수
+    func fetchByMonth() {
+        // TODO: 월 별 루틴 분류
+    }
+    
+    /// 루틴 부위 별 분류 함수
+    func fetchByPart() {
+        // TODO: 부위 별 루틴 분류
+    }
+    
+    /// 루틴 상세 정보 네비게이션 용 함수
+    func viewDetailedRoutine() {
+        // TODO: 선택한 루틴 상세 정보 보기
+    }
 }
