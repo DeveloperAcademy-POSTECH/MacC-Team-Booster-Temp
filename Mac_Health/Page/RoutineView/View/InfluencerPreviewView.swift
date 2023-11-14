4e8d66615eb5e529b817034449df2ab7628921ad
songmoro <wlqgkr@naver.com>
2023-11-15 04:16:34 +0900

    feat: 루틴 총합 뷰 분기 추가

---
+++ Mac_Health/Page/RoutineView/View/InfluencerPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  InfluencerPreviewView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/15/23.
+//
+
+import SwiftUI
+
+struct InfluencerPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfluencerPreviewView()
+}
--- Mac_Health/Page/RoutineView/View/RoutineView.swift
+++ Mac_Health/Page/RoutineView/View/RoutineView.swift
@@ -7,9 +7,24 @@
 
 import SwiftUI
 
+// TODO: vm 추가 - 인플루언서 구독 조회
 struct RoutineView: View {
+    @State var influencerIds: [Int] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch influencerIds.count {
+            /// 구독 없을 때 보여줄 뷰
+        case 0:
+            InfluencerPreviewView()
+            
+            /// 구독 한 명일 때 보여줄 뷰
+        case 1:
+//            TodayStartView(routine:)
+            EmptyView()
+            /// 구독 여러 명일 때 보여줄 뷰
+        default:
+            MultiRoutineView()
+        }
     }
 }
 
