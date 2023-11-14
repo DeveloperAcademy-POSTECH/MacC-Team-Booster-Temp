1f67e7b89e39308fbabda5eb0ada721d0b225560
songmoro <wlqgkr@naver.com>
2023-11-15 04:37:36 +0900

    style: 함수 설명 추가

--- Mac_Health/Page/Main/ViewModel/MainViewModel.swift
+++ Mac_Health/Page/Main/ViewModel/MainViewModel.swift
@@ -10,18 +10,22 @@ import SwiftUI
 class MainViewModel: ObservableObject {
     @Published var tabSelection = 0
     
+    /// 루틴 탭 이동
     func changeToRoutineTab() {
         tabSelection = 0
     }
     
+    /// 둘러보기 탭 이동
     func changeToSearchTab() {
         tabSelection = 1
     }
     
+    /// 기록 탭 이동
     func changeToRecordTab() {
         tabSelection = 2
     }
     
+    /// 프로필 탭 이동
     func changeToProfileTab() {
         tabSelection = 3
     }
