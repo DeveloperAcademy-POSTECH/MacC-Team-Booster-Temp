ccbac99da6fe865d1d30ed67b6d500a9d0ccf218
songmoro <wlqgkr@naver.com>
2023-11-15 04:18:14 +0900

    style: 투두 추가

--- Mac_Health/Page/RoutineView/View/InfluencerPreviewView.swift
+++ Mac_Health/Page/RoutineView/View/InfluencerPreviewView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// TODO: 둘러보기 뷰 작업
+/// 구독한 인플루언서가 없을 때 보여지는 뷰
 struct InfluencerPreviewView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
