332e161d6734be831c8c1cdc7c2b78e8210a459e
songmoro <wlqgkr@naver.com>
2023-11-15 06:44:00 +0900

    feat: 운동 정보 뷰 모델 함수 추가

--- Mac_Health/Page/RoutineView/ViewModel/RoutineInformationViewModel.swift
+++ Mac_Health/Page/RoutineView/ViewModel/RoutineInformationViewModel.swift
@@ -8,5 +8,21 @@
 import SwiftUI
 
 class RoutineInformationViewModel: ObservableObject {
+    /// 루틴 목록
+    @Published var routine = ""
     
+    /// 루틴 정보 조회 함수
+    func fetchRoutine() {
+        // TODO: 루틴 목록 조회
+    }
+    
+    /// 루틴 부위 별 분류 함수
+    func fetchByPart() {
+        // TODO: 부위 별 루틴 분류
+    }
+    
+    /// 운동 시작하기 위한 운동 목록 편집 네비게이션 용 함수
+    func startWorkout() {
+        // TODO: 운동 시작하기
+    }
 }
