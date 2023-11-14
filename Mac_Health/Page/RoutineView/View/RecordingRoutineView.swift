692d374b4d5126e870d71226020bd946a5f63941
songmoro <wlqgkr@naver.com>
2023-11-15 07:17:09 +0900

    feat: 운동 기록 중 목록 확인 뷰 추가

---
+++ Mac_Health/Page/RoutineView/View/RecordingRoutineView.swift
@@ -0,0 +1,19 @@
+//
+//  RecordingRoutineView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/15/23.
+//
+
+import SwiftUI
+
+/// 운동 기록 중 운동 목록을 보기 위한 뷰
+struct RecordingRoutineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordingRoutineView()
+}
