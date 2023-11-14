394b8c55559d59d517c8cae8476e5415a54340d4
songmoro <wlqgkr@naver.com>
2023-11-15 05:29:10 +0900

    style: 투두 추가

--- Mac_Health/Page/RoutineView/View/SingleInfluencerRoutineView.swift
+++ Mac_Health/Page/RoutineView/View/SingleInfluencerRoutineView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// TODO: 인플루언서 아이디 받아서 조회 후 데이터 뿌려주기
 struct SingleInfluencerRoutineView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
