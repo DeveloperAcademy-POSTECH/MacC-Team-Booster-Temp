a90d564da0f47c5ae68712572e59194e16c04e95
songmoro <wlqgkr@naver.com>
2023-11-15 06:40:26 +0900

    feat: 운동 목록 뷰 모델 추가

---
+++ Mac_Health/Page/RoutineView/ViewModel/RoutineInformationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RoutineInformationViewModel.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/15/23.
+//
+
+import SwiftUI
+
+class RoutineInformationViewModel: ObservableObject {
+    
+}l
