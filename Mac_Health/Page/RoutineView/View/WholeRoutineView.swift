410043cd4d77deea1f59091c52c628c42ec0a5da
songmoro <wlqgkr@naver.com>
2023-11-15 06:06:26 +0900

    feat: 전체 운동 뷰 추가

---
+++ Mac_Health/Page/RoutineView/View/WholeRoutineView.swift
@@ -0,0 +1,19 @@
+//
+//  WholeRoutineView.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/15/23.
+//
+
+import SwiftUI
+
+/// 인플루언서 개인의 전체 루틴 뷰
+struct WholeRoutineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WholeRoutineView()
+}
