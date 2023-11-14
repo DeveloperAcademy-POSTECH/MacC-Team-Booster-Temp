212d86219291483b9d3b999f23c2f4ac7e6ea841
songmoro <wlqgkr@naver.com>
2023-11-15 04:26:01 +0900

    feat: 메인 뷰 모델 추가

---
+++ Mac_Health/Page/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  Mac_Health
+//
+//  Created by 송재훈 on 11/15/23.
+//
+
+import SwiftUI
+
+class MainViewModel: ObservableObject {
+    @Published var tabSelection = 1
+}
