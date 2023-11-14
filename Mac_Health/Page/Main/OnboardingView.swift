dbe8f0c5a76764fb9ffa8ac315be4459867ce057
songmoro <wlqgkr@naver.com>
2023-11-15 03:32:34 +0900

    feat: 온보딩 컴포넌트화

--- Mac_Health/Page/Main/OnboardingView.swift
+++ Mac_Health/Page/Main/OnboardingView.swift
@@ -13,58 +13,64 @@ struct OnboardingView: View {
     @State var isPass = false
     
     var body: some View {
-        // 로그인 성공 시
-        if isPass {
+        if !isPass {
+            // 로그인 전
+            Onboarding
+        }
+        else {
+            // 로그인 성공 시
             MainView()
         }
-        // 로그인 전
-        else {
-            ZStack {
-                // TODO: 색 변경
-                Color.gray.ignoresSafeArea()
-                
-                // TODO: 사이즈 변경
-                FloatingButton(backgroundColor: .clear) {
-                    SignInWithAppleButton(.signIn) { request in
-                        request.requestedScopes = [.email]
-                    } onCompletion: { results in
-                        // TODO: 추후 vm 생성
-                        switch results {
-                        case .success(let result):
-                            switch result.credential {
-                            case let userCredential as ASAuthorizationAppleIDCredential:
-                                let identifier = userCredential.user
-                                let identityToken = String(data: userCredential.identityToken!, encoding: .utf8)
-                                let authorizationCode = String(data: userCredential.authorizationCode!, encoding: .utf8)
-                                
-                                postLogin(identifier: identifier, identityToken: identityToken!, authorizationCode: authorizationCode!)
-                            default:
-                                break
-                            }
-                        case .failure(let error):
-                            print(error.localizedDescription)
+    }
+    
+    // TODO: 뷰 디테일 작업
+    var Onboarding: some View {
+        ZStack {
+            // TODO: 색 변경
+            Color.gray.ignoresSafeArea()
+            
+            // TODO: 사이즈 변경
+            FloatingButton(backgroundColor: .clear) {
+                SignInWithAppleButton(.signIn) { request in
+                    request.requestedScopes = [.email]
+                } onCompletion: { results in
+                    // TODO: 추후 vm 생성
+                    switch results {
+                    case .success(let result):
+                        switch result.credential {
+                        case let userCredential as ASAuthorizationAppleIDCredential:
+                            let identifier = userCredential.user
+                            let identityToken = String(data: userCredential.identityToken!, encoding: .utf8)
+                            let authorizationCode = String(data: userCredential.authorizationCode!, encoding: .utf8)
+                            
+                            postLogin(identifier: identifier, identityToken: identityToken!, authorizationCode: authorizationCode!)
+                        default:
+                            break
                         }
+                    case .failure(let error):
+                        print(error.localizedDescription)
                     }
-                    .cornerRadius(100)
-                    .signInWithAppleButtonStyle(.white)
                 }
+                .cornerRadius(100)
+                .signInWithAppleButtonStyle(.white)
             }
-            //        .onAppear {
-            //            let appleIDProvider = ASAuthorizationAppleIDProvider()
-            //            appleIDProvider.getCredentialState(forUserID: "") { (credentialState, error) in
-            //                switch credentialState {
-            //                    case .authorized:
-            //                       print("authorized")
-            //                    case .revoked:
-            //                       print("revoked")
-            //                    case .notFound:
-            //                       print("notFound")
-            //                    default:
-            //                        break
-            //                }
-            //            }
-            //        }
+            // TODO: 둘러보기 버튼 추가
         }
+        //        .onAppear {
+        //            let appleIDProvider = ASAuthorizationAppleIDProvider()
+        //            appleIDProvider.getCredentialState(forUserID: "") { (credentialState, error) in
+        //                switch credentialState {
+        //                    case .authorized:
+        //                       print("authorized")
+        //                    case .revoked:
+        //                       print("revoked")
+        //                    case .notFound:
+        //                       print("notFound")
+        //                    default:
+        //                        break
+        //                }
+        //            }
+        //        }
     }
     
     /// 애플 로그인 성공 시 서버에 액세스 토큰 요청 함수
