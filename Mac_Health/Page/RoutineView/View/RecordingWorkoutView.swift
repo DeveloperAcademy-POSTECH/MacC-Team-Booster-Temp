2dc6e291c2dae96e97fb0be0add7da857605eb6c
songmoro <wlqgkr@naver.com>
2023-11-15 17:16:47 +0900

    feat: 운동 기록 뷰 바인딩, 뷰 문서화

--- Mac_Health/Page/RoutineView/View/RecordingWorkoutView.swift
+++ Mac_Health/Page/RoutineView/View/RecordingWorkoutView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 /// 운동 시작 했을 때 기록하기 위한 뷰
+/// - Parameters:
+///  - routineId: 정보를 조회할 루틴의 id
+///  - exerciseId: 정보를 조회할 루틴의 운동 id
 struct RecordingWorkoutView: View {
+    let routineId: Int
+    let exerciseId: Int
+    @StateObject var vm = RecordingWorkoutViewModel()
+    
     var body: some View {
         Text("RecordingWorkoutView")
     }
 }
 
 #Preview {
-    RecordingWorkoutView()
+    RecordingWorkoutView(routineId: 2, exerciseId: 7)
 }
