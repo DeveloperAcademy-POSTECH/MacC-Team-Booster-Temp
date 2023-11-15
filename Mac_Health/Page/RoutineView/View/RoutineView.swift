f4ba42883dd33d728061d0d0ea46b664bf1c88c0
songmoro <wlqgkr@naver.com>
2023-11-15 15:42:03 +0900

    feat: 루틴 총합 뷰 뷰 모델 바인딩

--- Mac_Health/Page/RoutineView/View/RoutineView.swift
+++ Mac_Health/Page/RoutineView/View/RoutineView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 // TODO: vm 추가 - 인플루언서 구독 조회
 /// 루틴 탭 최상위 뷰
 struct RoutineView: View {
-    @State var influencerIds: [Int] = []
+    @StateObject var vm = RoutineViewModel()
     
     var body: some View {
-        switch influencerIds.count {
+        switch vm.routines.routines.count {
         case 0: /// 구독 없을 때 보여줄 뷰
             InfluencerPreviewView()
         case 1: /// 구독 한 명일 때 보여줄 뷰
--- Mac_Health/Page/RoutineView/ViewModel/RoutineViewModel.swift
+++ Mac_Health/Page/RoutineView/ViewModel/RoutineViewModel.swift
@@ -8,5 +8,24 @@
 import SwiftUI
 
 class RoutineViewModel: ObservableObject {
+    @Published var routines = ResponseGetUsersRoutines(routines: [])
     
+    init() {
+        self.fetchRoutines()
+    }
+    
+    func fetchRoutines() {
+        // TODO: date 오늘 날짜로
+        let date = "2023-10-24"
+        
+        GeneralAPIManger.request(for: .GetUsersRoutines(date: date), type: [InfluencerRoutine].self) {
+            switch $0 {
+            case .success(let routines):
+                self.routines.routines = routines
+                print(self.routines)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
