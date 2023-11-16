c726f102b3d39c9a991b0a9e2486b632eaa5fea7
CHUNG Hoiseong <pmchung423@gmail.com>
2023-11-16 14:06:01 +0900

    add profileviewmodel

--- Mac_Health/Page/ProfileView/ViewModel/ProfileViewModel.swift
+++ Mac_Health/Page/ProfileView/ViewModel/ProfileViewModel.swift
@@ -10,6 +10,12 @@ import SwiftUI
 class ProfileViewModel: ObservableObject {
     ///현재 이름
     //TODO: nickname에 서버 User name 받아오기 - MORO
+    var tasteName:[String] = ["할라피뇨맛", "아몬드맛", "쿠앤크맛", "솜사탕맛", "고추가루맛", "초콜릿맛", "바닐라맛", "딸기맛", "민트맛", "카라멜맛", "바나나맛", "오렌지맛", "레몬맛", "라즈베리맛", "블루베리맛", "애플맛", "파인애플맛", "키위맛", "체리맛", "자몽맛", "산딸기맛", "피치맛", "망고맛", "코코넛맛", "라임맛", "크림맛", "캐러멜맛", "헤이즐넛맛", "버터맛", "크랜베리맛", "피넛버터맛", "아몬드맛", "카라멜맛", "모카맛", "아이스크림맛", "민트초코맛", "치즈케이크맛", "요거트맛", "오레오맛", "메론맛", "아이스티맛", "청포도맛","애플파이맛", "체리콜라맛", "마끼아또맛", "허니버터맛"]
+    var workoutName:[String] = ["프로틴","덤벨","케틀벨","아령","사레레","단백질","풀업","벤치프레스", "레그프레스","스쿼트","바벨","풀업","푸시업","크런치","플랭크","런지","스트랩","폼롤러","천국의계단","유산소","헬스장","인터벌","타바타","숄더프레스","바벨컬","덤벨로우","레그컬","스트레칭","드랍세트","닭가슴살","랫풀다운","하체","이두","삼두","전완근","견갑","복근","승모근","전거근","삼각근","내전근","외복사근","대흉근","광배근","대퇴이두근","둔근","오운완","프리웨이트","트레드밀","수분조절"]
+    init() {
+        nickname = MakeName(taste: tasteName, workout: workoutName)
+    }
+    
     @Published var nickname = "정회승"
     @Published var notiToggle: Bool = true
     @Published var versionState: String = "1.0.0"
@@ -18,6 +24,14 @@ class ProfileViewModel: ObservableObject {
     
     @Published var deletingAccount = false
     @Published var loggingOutSheet = false
+    
+    //랜덤 이름 만들기
+    func MakeName(taste: [String], workout: [String]) -> String {
+        var rnd1 = Int.random(in: 0...45)
+        var rnd2 = Int.random(in: 0...50)
+        var rndName = taste[rnd1] + workout[rnd2]
+        return rndName
+    }
     //이름 가져오기
     func GetName(name: String) {
 
