987cac50e6e5e9ba2c62a73751f8123d59eba1d8
songmoro <wlqgkr@naver.com>
2023-11-18 03:58:01 +0900

    feat: 단일 인플루언서 둘러보기 뷰, 뷰 모델 추가

--- Mac_Health/Page/SearchView/ViewModel/SingleInfluencerPreviewViewModel.swift
+++ Mac_Health/Page/SearchView/ViewModel/SingleInfluencerPreviewViewModel.swift
@@ -7,12 +7,8 @@
 
 import SwiftUI
 
-struct SingleInfluencerPreviewViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    SingleInfluencerPreviewViewModel()
+class SingleInfluencerPreviewViewModel: ObservableObject {
+    @Published var mailData = ComposeMailData(subject: "비플 문의하기", recipients: ["pmchung423@gmail.com"], message: "비플 문의하기", attachments: [ AttachmentData(data: "Some text".data(using: .utf8)!, mimeType: "text/plain", fileName: "text.txt")])
+    
+    @Published var showMailView = false
 }
