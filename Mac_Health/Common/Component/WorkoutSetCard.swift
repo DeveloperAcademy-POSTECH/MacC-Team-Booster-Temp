90b2a569dbda21adfb7ae9a077c83ae750b82322
songmoro <wlqgkr@naver.com>
2023-11-21 01:30:48 +0900

    feat: 세트 조절 함수 분리

--- Mac_Health/Common/Component/WorkoutSetCard.swift
+++ Mac_Health/Common/Component/WorkoutSetCard.swift
@@ -68,7 +68,7 @@ struct WorkoutSetCard: View {
                         .multilineTextAlignment(.trailing)
                         .padding(.trailing, 10)
                         .onChange(of: debouncedStream.debouncedText) { weight in
-                                recordingWorkoutVM.editSet(index: index - 1, routineId: routineId, exerciseId: exerciseId, setId: set.setId, weight: weight, reps: set.reps) {
+                            recordingWorkoutVM.editWeight(index: index - 1, routineId: routineId, exerciseId: exerciseId, setId: set.setId, weight: weight, reps: set.reps) {
                                     set.weight = $0.weight
                             }
                         }
@@ -88,7 +88,7 @@ struct WorkoutSetCard: View {
                         .multilineTextAlignment(.trailing)
                         .padding(.trailing, 10)
                         .onChange(of: debouncedStream.debouncedReps) { reps in
-                            recordingWorkoutVM.editSet(index: index - 1, routineId: routineId, exerciseId: exerciseId, setId: set.setId, weight: set.weight ?? 0, reps: reps) {
+                            recordingWorkoutVM.editReps(index: index - 1, routineId: routineId, exerciseId: exerciseId, setId: set.setId, weight: set.weight ?? 0, reps: reps) {
                                 set.reps = $0.reps
                             }
                         }
