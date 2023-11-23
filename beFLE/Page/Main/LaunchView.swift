7efa41a5ba1e3744198cac9bfa59d2d4d5d4c265
songmoro <wlqgkr@naver.com>
2023-11-23 19:41:35 +0900

    feat: 런치 뷰 추가

---
+++ beFLE/Page/Main/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  beFLE
+//
+//  Created by 송재훈 on 11/23/23.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
