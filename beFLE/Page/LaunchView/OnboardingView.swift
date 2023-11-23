5befe3ca60a125ce8e0ea0d88befd92f55e4336d
songmoro <wlqgkr@naver.com>
2023-11-23 19:53:47 +0900

    feat: 런치 스크린 컴포넌트 삭제, 관련 코드 주석 처리

--- beFLE/Page/LaunchView/OnboardingView.swift
+++ beFLE/Page/LaunchView/OnboardingView.swift
@@ -38,16 +38,16 @@ struct OnboardingView: View {
                 MainView()
             }
             
-            if isLoading {
-                LaunchScreen()
-                    .transition(.opacity)
-                    .zIndex(1)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 4, execute: {
-                            withAnimation { isLoading.toggle() }
-                        })
-                    }
-            }
+//            if isLoading {
+//                LaunchScreen()
+//                    .transition(.opacity)
+//                    .zIndex(1)
+//                    .onAppear {
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 4, execute: {
+//                            withAnimation { isLoading.toggle() }
+//                        })
+//                    }
+//            }
         }
     }
     
