031a2490c0434fef8a77ac27d04f169220bdbdce
songmoro <wlqgkr@naver.com>
2023-11-23 19:50:38 +0900

    feat: 런치 뷰에 모델 추가

--- beFLE/Page/LaunchView/LaunchView.swift
+++ beFLE/Page/LaunchView/LaunchView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @StateObject var vm = LaunchViewModel.shared
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
