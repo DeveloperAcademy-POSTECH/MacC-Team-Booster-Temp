a3c39fb8e4d87ead23348400c8c92615a4369354
CHUNG Hoiseong <pmchung423@gmail.com>
2023-11-24 23:39:03 +0900

    mock up routine view refac

--- beFLE/Page/StartMockUpView/View/Routine/MockUpFinishView.swift
+++ beFLE/Page/StartMockUpView/View/Routine/MockUpFinishView.swift
@@ -19,92 +19,102 @@ struct MockUpFinishView: View {
             VStack (spacing: 0) {
                 Spacer()
                 Spacer()
-                
-                VStack(spacing: 10){
-                    Text("\(getNowDateTime()) 운동 완료")
-                        .font(.title1())
-                        .foregroundColor(.label_900)
-                    Text("오늘도 고생 많으셨어요!")
-                        .font(.body())
-                        .foregroundColor(.label_700)
-                }
-                .frame(height: UIScreen.getHeight(80))
-                
+                finishDate
                 Spacer()
-                
-                Image("finishImage")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: UIScreen.getWidth(318))
-                    .background{
-                        RoundedRectangle(cornerRadius: 8.0)
-                            .frame(width: UIScreen.getWidth(318))
-                            .foregroundColor(.gray_700)
-                    }
-                    .overlay{
-                        VStack{
-                            Spacer()
-                            HStack(spacing: 40){
-                                VStack(spacing: 3){
-                                    Text("\(String(timeFormatted(elapsedTime)))")
-                                        .font(.title2())
-                                        .foregroundColor(.label_900)
-                                    Text("운동시간")
-                                        .font(.body2())
-                                        .foregroundColor(.label_700)
-                                }
-                                VStack(spacing: 3){
-                                    Text("580kcal")
-                                        .font(.title2())
-                                        .foregroundColor(.label_900)
-                                    Text("소모칼로리")
-                                        .font(.body2())
-                                        .foregroundColor(.label_700)
-                                }
-                                VStack(spacing: 3){
-                                    Text("9840kg")
-                                        .font(.title2())
-                                        .foregroundColor(.label_900)
-                                    Text("총 볼륨")
-                                        .font(.body2())
-                                        .foregroundColor(.label_700)
-                                }
-                            }
-                        }
-                        .padding(.bottom, 30)
-                    }
-                
+                finishImage
                 Spacer()
                 Spacer()
-                
-                Button{
-                    MockUpMainViewModel.shared.changeToRecordTab()
-                    MockUpMainViewModel.shared.resetNavigationStack()
-                } label: {
-                    FloatingButton(size: .medium, color: .green_main) {
-                        Text("기록 확인")
-                            .foregroundColor(.gray_900)
-                            .font(.button1())
-                    }
-                    .padding(.bottom, 12)
-                }
-                
-                Button{
-                    //TODO: root 네비게이션으로 연결 대체
-                    LaunchViewModel.shared.previewLogin()
-                } label: {
-                    FloatingButton(size: .medium, color: .gray_600) {
-                        Text("로그인")
-                            .foregroundColor(.green_main)
-                            .font(.button1())
-                    }
-                    .padding(.bottom, 30)
-                }
+                showRecord
+                showLogin
             }
             .padding(.bottom, 30)
         }
         .navigationBarBackButtonHidden()
         .ignoresSafeArea()
+    }
+    
+    var finishDate: some View {
+        VStack(spacing: 10){
+            Text("\(getNowDateTime()) 운동 완료")
+                .font(.title1())
+                .foregroundColor(.label_900)
+            Text("오늘도 고생 많으셨어요!")
+                .font(.body())
+                .foregroundColor(.label_700)
+        }
+        .frame(height: UIScreen.getHeight(80))
+    }
+    
+    var finishImage: some View {
+        Image("finishImage")
+            .resizable()
+            .scaledToFit()
+            .frame(width: UIScreen.getWidth(318))
+            .background{
+                RoundedRectangle(cornerRadius: 8.0)
+                    .frame(width: UIScreen.getWidth(318))
+                    .foregroundColor(.gray_700)
+            }
+            .overlay{
+                VStack{
+                    Spacer()
+                    HStack(spacing: 40){
+                        VStack(spacing: 3){
+                            Text("\(String(timeFormatted(elapsedTime)))")
+                                .font(.title2())
+                                .foregroundColor(.label_900)
+                            Text("운동시간")
+                                .font(.body2())
+                                .foregroundColor(.label_700)
+                        }
+                        VStack(spacing: 3){
+                            Text("580kcal")
+                                .font(.title2())
+                                .foregroundColor(.label_900)
+                            Text("소모칼로리")
+                                .font(.body2())
+                                .foregroundColor(.label_700)
+                        }
+                        VStack(spacing: 3){
+                            Text("9840kg")
+                                .font(.title2())
+                                .foregroundColor(.label_900)
+                            Text("총 볼륨")
+                                .font(.body2())
+                                .foregroundColor(.label_700)
+                        }
+                    }
+                }
+                .padding(.bottom, 30)
+            }
+    }
+    
+    var showRecord: some View {
+        Button{
+            MockUpMainViewModel.shared.changeToRecordTab()
+            MockUpMainViewModel.shared.resetNavigationStack()
+        } label: {
+            FloatingButton(size: .medium, color: .green_main) {
+                Text("기록 확인")
+                    .foregroundColor(.gray_900)
+                    .font(.button1())
+            }
+            .padding(.bottom, 12)
+        }
+    }
+
+    var showLogin: some View {
+        Button{
+            //TODO: root 네비게이션으로 연결 대체
+            LaunchViewModel.shared.previewLogin()
+        } label: {
+            FloatingButton(size: .medium, color: .gray_600) {
+                Text("로그인")
+                    .foregroundColor(.green_main)
+                    .font(.button1())
+            }
+            .padding(.bottom, 30)
+        }
     }
     
     func getNowDateTime() -> String {
