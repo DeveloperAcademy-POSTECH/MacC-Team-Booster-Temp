e7562c9b70326d02d5bda775fb5b504f2609d8bd
songmoro <wlqgkr@naver.com>
2023-11-26 07:46:50 +0900

    feat: 컴포넌트 분리, 뷰 연결

--- beFLE/Page/RoutineView/View/EditRecordingRoutineView.swift
+++ beFLE/Page/RoutineView/View/EditRecordingRoutineView.swift
@@ -8,39 +8,20 @@
 import SwiftUI
 
 struct EditRecordingRoutineView: View {
-    let routineId: Int
-    
-    @EnvironmentObject var editRoutineVM: EditRoutineViewModel
-    
-    
+    @StateObject var vm = EditRecordingRoutineViewModel()
+    @EnvironmentObject var workoutVM: WorkoutViewModel
     @Environment(\.dismiss) var dismiss: DismissAction
-    var burnedKCalories: Int
+    
     var body: some View {
-        VStack {
+        ZStack {
             WorkoutList
         }
-        .navigationTitle("운동 루틴 편집")
+        .navigationTitle("운동 목록 편집")
+        .navigationBarBackButtonHidden()
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 BackButton
             }
-        }
-        .navigationBarBackButtonHidden()
-        .sheet(isPresented: $editRoutineVM.isDetailedWorkoutSheetShow) {
-//            DetailedWorkoutSheet(routineId: routineId, exerciseId: editRoutineVM.routine.exercises[editRoutineVM.selectedIndex].id)
-        }
-        .confirmationDialog(editRoutineVM.routine.exercises.isEmpty ? "" : editRoutineVM.routine.exercises[editRoutineVM.selectedIndex].name , isPresented: $editRoutineVM.isEditWorkoutActionShow, titleVisibility: .visible) {
-            AlternativeActionSheet
-        }
-        .sheet(isPresented: $editRoutineVM.isAlternateWorkoutSheetShow) {
-            AlternateWorkoutSheet(routineId: routineId, exerciseId: editRoutineVM.routine.exercises[editRoutineVM.selectedIndex].id)
-                .environmentObject(editRoutineVM)
-                .onDisappear{
-                    editRoutineVM.fetchRoutine(routineId: routineId)
-                }
-        }
-        .alert("운동을 삭제하시겠습니까?", isPresented: $editRoutineVM.isDeleteWorkoutAlertShow) {
-            DeleteAlert
         }
     }
 }
@@ -49,7 +30,7 @@ struct EditRecordingRoutineView: View {
 extension EditRecordingRoutineView {
     var BackButton: some View {
         Button {
-            dismiss()
+            workoutVM.changeViewStatus(.recordingRoutineView)
         } label: {
             Image(systemName: "chevron.left")
                 .foregroundColor(.label_700)
@@ -58,40 +39,46 @@ extension EditRecordingRoutineView {
     }
 }
 
-/// 운동 정보
+/// 운동 목록
 extension EditRecordingRoutineView {
     var WorkoutList: some View {
         VStack {
             HStack {
-                Text(editRoutineVM.routine.part)
+                Text(workoutVM.routine.part)
                     .foregroundColor(.label_900)
                     .font(.headline1())
+                    .padding([.horizontal, .top])
                 
                 Spacer()
             }
             
             ScrollView {
-                ForEach(0..<editRoutineVM.routine.exercises.count, id: \.self) { index in
-                    WorkoutListCell(index: index)
+                ForEach(Array(workoutVM.routine.exercises.enumerated()), id: \.element) { pair in
+                    WorkoutListCell(pair.offset, pair.element)
+                        .padding(.vertical, 4)
                 }
+                .padding(.horizontal)
+                FloatingButton(size: .medium) {}
+                    .padding()
             }
-            .scrollIndicators(.hidden)
+            
         }
-        .padding([.top, .horizontal])
+        .padding(.horizontal)
     }
     
-    func WorkoutListCell(index: Int) -> some View {
+    @ViewBuilder
+    func WorkoutListCell(_ index: Int, _ exercise: Exercise) -> some View {
         HStack {
             Button {
-                editRoutineVM.selectedIndex = index
-                editRoutineVM.isDetailedWorkoutSheetShow = true
+                vm.isDetailedWorkoutSheetShow = true
+                vm.selectedExerciseId = exercise.id
             } label: {
-                HStack {
+                HStack(spacing: 8) {
                     RoundedRectangle(cornerRadius: 4)
                         .foregroundColor(.fill_1)
                         .frame(width: UIScreen.getWidth(64), height: UIScreen.getHeight(64))
                         .overlay {
-                            AsyncImage(url: URL(string: editRoutineVM.routine.exercises[index].exerciseImageUrl)) { image in
+                            AsyncImage(url: URL(string: exercise.exerciseImageUrl)) { image in
                                 image
                                     .resizable()
                                     .scaledToFit()
@@ -101,62 +88,79 @@ extension EditRecordingRoutineView {
                             }
                         }
                     
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text(editRoutineVM.routine.exercises[index].name)
+                    VStack(alignment: .leading, spacing: 3) {
+                        Text(exercise.name)
                             .foregroundColor(.label_900)
                             .font(.headline1())
                             .multilineTextAlignment(.leading)
                             .allowsTightening(true)
-                        HStack (spacing: 6) {
-                            Text("\(editRoutineVM.routine.exercises[index].numberOfSet)세트")
+                        HStack(spacing: 3){
+                            Text("\(exercise.numberOfSet)세트")
                                 .foregroundColor(.label_700)
                                 .font(.body2())
                             Text("|")
                                 .foregroundColor(.label_400)
                                 .font(.body2())
                                 .scaleEffect(0.8)
-                            Text("\(editRoutineVM.routine.exercises[index].recommendReps)회")
+                            Text("\(exercise.recommendReps)회")
                                 .foregroundColor(.label_700)
                                 .font(.body2())
                         }
                     }
+                    .padding()
                     
                     Spacer()
                 }
+            }
+            .sheet(isPresented: $vm.isDetailedWorkoutSheetShow) {
+                DetailedWorkoutSheet(routineId: workoutVM.routineId, exerciseId: vm.selectedExerciseId)
             }
             
             Spacer()
             
             Button {
-                editRoutineVM.selectedIndex = index
-                editRoutineVM.isEditWorkoutActionShow = true
+                vm.isEditWorkoutActionShow = true
+                vm.editWorkoutName = exercise.name
+                vm.selectedExerciseId = exercise.id
             } label: {
                 Image(systemName: "ellipsis")
                     .foregroundColor(.label_700)
             }
+            .padding()
+            .confirmationDialog(vm.editWorkoutName, isPresented: $vm.isEditWorkoutActionShow, titleVisibility: .visible) {
+                AlternativeActionSheet
+            }
+            .sheet(isPresented: $vm.isAlternateWorkoutSheetShow) {
+                AlternateWorkoutSheet(routineId: workoutVM.routineId, exerciseId: vm.selectedExerciseId)
+                    .onDisappear {
+                        workoutVM.fetchRoutine()
+                    }
+            }
+            .alert("운동을 삭제하시겠습니까?", isPresented: $vm.isDeleteWorkoutAlertShow) {
+                DeleteAlert
+            }
         }
-        .padding(.vertical, 4)
     }
 }
 
-/// 얼럿, 액션
+/// 시트, 얼럿, 액션
 extension EditRecordingRoutineView {
     @ViewBuilder
     var AlternativeActionSheet: some View {
         Button {
-            editRoutineVM.isAlternateWorkoutSheetShow = true
+            vm.isAlternateWorkoutSheetShow = true
         } label: {
             Text("운동 대체")
         }
         
-        Button {
-            editRoutineVM.isDeleteWorkoutAlertShow = true
+        Button(role: .destructive) {
+            vm.isDeleteWorkoutAlertShow = true
         } label: {
             Text("삭제")
         }
         
         Button(role: .cancel) {
-
+            
         } label: {
             Text("취소")
         }
@@ -164,11 +168,8 @@ extension EditRecordingRoutineView {
     
     @ViewBuilder
     var DeleteAlert: some View {
-        Button("취소") {
-
-        }
-        Button("삭제") {
-            editRoutineVM.deleteWorkout(routineId: routineId, exerciseId: editRoutineVM.workout.exerciseId)
+        Button("삭제", role: .destructive) {
+            vm.deleteWorkout(routineId: workoutVM.routineId, exerciseId: vm.selectedExerciseId)
         }
     }
 }
