15cd380dd6d17b27f993961221eded235fded73c
songmoro <wlqgkr@naver.com>
2023-11-25 14:30:11 +0900

    feat: WorkoutView 추가

---
+++ beFLE/Page/RoutineView/View/WorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutView.swift
+//  beFLE
+//
+//  Created by 송재훈 on 11/25/23.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutView()
+}
