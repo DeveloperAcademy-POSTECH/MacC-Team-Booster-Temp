0a7bd874d1a5159c1c7334c922d01ff4e843b5f6
songmoro <wlqgkr@naver.com>
2023-11-26 07:36:15 +0900

    feat: 뷰모델 추가

---
+++ beFLE/Page/RoutineView/ViewModel/EditRecordingRoutineViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  EditRecordingRoutineViewModel.swift
+//  beFLE
+//
+//  Created by 송재훈 on 11/26/23.
+//
+
+import SwiftUI
+
+struct EditRecordingRoutineViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditRecordingRoutineViewModel()
+}
