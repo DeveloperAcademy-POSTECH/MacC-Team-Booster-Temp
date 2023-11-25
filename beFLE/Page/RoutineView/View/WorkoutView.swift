7acaef64a1432d53ae71c0a79722594976ed8eb0
songmoro <wlqgkr@naver.com>
2023-11-25 15:10:38 +0900

    feat: 뷰 상태 열거 추가

--- beFLE/Page/RoutineView/View/WorkoutView.swift
+++ beFLE/Page/RoutineView/View/WorkoutView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+enum WorkoutViewStatus {
+    case EditRoutineView
+    case RecordingWorkoutView
+    case RecordingRoutineView
+    case EditRecordingRoutineView
+    case RecordingFinishView
+}
+
 struct WorkoutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
