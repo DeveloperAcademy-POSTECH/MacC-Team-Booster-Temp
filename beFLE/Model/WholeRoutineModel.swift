ff00f67a346d4263557eeac7ecf4bedc1777e8d1
songmoro <wlqgkr@naver.com>
2023-11-29 02:01:09 +0900

    feat: 전체 운동 목록 모델 추가

--- beFLE/Model/WholeRoutineModel.swift
+++ beFLE/Model/WholeRoutineModel.swift
@@ -8,4 +8,15 @@
 import SwiftUI
 
 struct WholeRoutineModel {
+    func fetchWholeRoutine(influencerId: Int, completion: @escaping ((ResponseGetUsersInfluencersRoutines)->()) ) {
+        GeneralAPIManger.request(for: .GetUsersInfluencersRoutines(id: influencerId), type: [Routine].self) {
+            switch $0 {
+            case .success(let routine):
+                var wholeRoutine = ResponseGetUsersInfluencersRoutines(routines: routine)
+                completion(wholeRoutine)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
