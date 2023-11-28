b556e1d89c3aa675a6ffd475473a2231b54f7550
CHUNG Hoiseong <pmchung423@gmail.com>
2023-11-28 14:22:37 +0900

    fix calKcal

--- beFLE/Page/RoutineView/ViewModel/RecordingFinishViewModel.swift
+++ beFLE/Page/RoutineView/ViewModel/RecordingFinishViewModel.swift
@@ -23,7 +23,8 @@ class RecordingFinishViewModel: ObservableObject {
             case .success(let routine):
                 _ = routine.exercises.map { exercise in
                     self.fetchWorkout(routineId: routineId, exerciseId: exercise.id) {
-                        self.volume += $0
+                        self.volume += $0.first ?? 0
+                        self.totalCalories += $0.last ?? 0
                     }
                 }
             case .failure(let error):
@@ -33,35 +34,25 @@ class RecordingFinishViewModel: ObservableObject {
         }
     }
     
-    func caculateburnedKCalories(routineId: Int) {
-        GeneralAPIManger.request(for: .GetUsersRoutinesId(id: routineId), type: ResponseGetUsersRoutinesId.self) {
-            switch $0 {
-            case .success(let routine):
-                _ = routine.exercises.map { exercise in
-                    self.fetchWorkout(routineId: routineId, exerciseId: exercise.id) {_ in 
-                        let caloriesPerSet: Double = 16.7
-                        let setsCount = exercise.numberOfSet
-                        let exerciseCalories = Int(Double(setsCount) * caloriesPerSet)
-                        self.totalCalories += exerciseCalories
-                    }
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    func fetchWorkout(routineId: Int, exerciseId: Int, completion: @escaping ((Int) -> ()) ) {
+    func fetchWorkout(routineId: Int, exerciseId: Int, completion: @escaping (([Int]) -> ()) ) {
         GeneralAPIManger.request(for: .GetRoutinesExercises(routineId: routineId, exerciseId: exerciseId), type: ResponseGetRoutinesExercises.self) {
             switch $0 {
             case .success(let workout):
                 var volume = 0
+                
+                let caloriesPerSet: Double = 16.7
+                var totalCalories: Double = 0
+                
                 _ = workout.sets.map {
                     if let weight = $0.weight {
                         volume += $0.reps * weight
                     }
+
+                    if $0.isDone {
+                        totalCalories += caloriesPerSet
+                    }
                 }
-                completion(volume)
+                completion([volume, Int(totalCalories)])
             case .failure(let error):
                 print(error.localizedDescription)
             }
