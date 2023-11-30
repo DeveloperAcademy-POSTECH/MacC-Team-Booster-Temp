4e37728dcadf897ef87745aa1c25ee50bdcfadb3
songmoro <wlqgkr@naver.com>
2023-11-30 13:49:42 +0900

    feat: 운동 모델 init

---
+++ beFLE/Model/ExerciseModel.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseModel.swift
+//  beFLE
+//
+//  Created by 송재훈 on 11/30/23.
+//
+
+import SwiftUI
+
+struct ExerciseModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseModel()
+}
