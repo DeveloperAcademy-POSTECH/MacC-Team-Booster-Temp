6432a6caa32d78da9272b9f62f4b32014f44a21a
songmoro <wlqgkr@naver.com>
2023-11-30 10:04:19 +0900

    feat: 로딩 이미지 컴포넌트 추가

---
+++ beFLE/Common/Component/LoadingImage.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingImage.swift
+//  beFLE
+//
+//  Created by 송재훈 on 11/30/23.
+//
+
+import SwiftUI
+
+struct LoadingImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingImage()
+}
