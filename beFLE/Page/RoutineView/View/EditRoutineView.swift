17bd93f83ec5c42f8108a789e81d160aab7a231e
CHUNG Hoiseong <pmchung423@gmail.com>
2023-12-02 01:15:52 +0900

    fix convention

--- beFLE/Page/RoutineView/View/EditRoutineView.swift
+++ beFLE/Page/RoutineView/View/EditRoutineView.swift
@@ -42,7 +42,7 @@ struct EditRoutineView: View {
                 }
         )
         .offset(x: vm.offset)
-        .animation(.easeInOut, value: vm.offset)
+        .animation(.linear, value: vm.offset)
         
     }
 }
